ec3a3860e43560029ebf797de2784787673cc8c4
Naveenaidu <naveennaidu479@gmail.com>
2018-11-02 16:01:34 +0530

    asda

--- scripts/import_csv.py
+++ scripts/import_csv.py
@@ -20,14 +20,14 @@ def main():
     their respective teams.
     """
 
-    csv_file            = "shortlist.csv"
+    csv_file            = "CoreCrewDetails.csv"
     team_count          = 0
     participant_count   = 0
 
 
     #Delete all existing teams and participants from the database.
-    Team.objects.all().delete()
-    Participant.objects.all().delete()
+    #Team.objects.all().delete()
+    #Participant.objects.all().delete()
 
     with open(csv_file) as f:
         reader = csv.reader(f)
@@ -41,46 +41,15 @@ def main():
                 name=item[0].strip(),
             )
 
-            no_of_p = int(item[1])
-            print(item[1])
-            participant_count += no_of_p
 
             p1 = Participant.objects.create(
                 participant_id=get_pid(),
-                name=item[2].strip() + " " + item[3].strip(),
-                gender=item[4].strip(),
-                college=item[7].strip(),
-                email=item[5].strip(),
-                phone=str(item[6]),
+                name=item[1].strip() + " ",
+                phone=str(item[2]),
                 noc_submitted=True,
                 paid=True,
                 team=t
             )
-
-            p2 = Participant.objects.create(
-                participant_id=get_pid(),
-                name=item[8].strip() + " " +item[9].strip(),
-                gender=item[10].strip(),
-                college=item[13].strip(),
-                email=item[11].strip(),
-                phone=str(item[12]),
-                noc_submitted=True,
-                paid=True,
-                team=t
-            )
-
-            if no_of_p == 3:
-                p3 = Participant.objects.create(
-                    participant_id=get_pid(),
-                    name=item[14].strip() + " " +item[15].strip(),
-                    gender=item[16].strip(),
-                    college=item[19].strip(),
-                    email=item[17].strip(),
-                    phone=str(item[18]),
-                    noc_submitted=True,
-                    paid=True,
-                    team=t
-                )
 
     print("{} teams and {} participants imported.".format(team_count,
         participant_count))
